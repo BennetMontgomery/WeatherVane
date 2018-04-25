f32b1227535d7e30a0a126e674638433afa9ca1d
Bennet Montgomery <bennetlogan@gmail.com>
2018-04-25 05:05:42 -0400

    First and only commit for this project. Contains the actual script.

---
+++ WeatherVane.py
@@ -0,0 +1,63 @@
+import json
+from urllib3 import *
+import sys
+
+#load weather data for chosen city
+def loadData(city):
+    #instantiating url manager
+    manager = PoolManager(num_pools=1)
+    #reading in raw data from api for chosen city
+    httpsData = manager.request("GET", "http://api.openweathermap.org/data/2.5/weather?q=" + city + "&appid=cd6ab6e6294be26428ea871d641b75c6")
+    #converting raw data to python dictionary
+    data = json.loads(httpsData.data.decode('utf-8'))
+    #return result
+    return data
+
+#print results of weather query
+def printResults(data):
+    glasscap ='\033[1;37;40m---'
+    bulbedge = '\033[1;37;40m -' + '\033[1;31;40m++++++' + '\033[1;37;40m-'
+    bulbmid = '\033[1;37;40m-' + '\033[1;31;40m++++++++' + '\033[1;37;40m'
+    
+    #determine how much to fill the thermometer
+    #   empty thermometer: <= -30.15C
+    #   full thermometer: <= 69.85C
+    temp = data['main']['temp']
+    for i in range(0, 10):
+        bulbedge += '\033[1;37;40m---'
+        if temp > 243:
+            bulbmid += '\033[1;31;40m+++'
+            temp -= 10
+        else:
+            bulbmid += '   '
+    
+    #capping thermometer end
+    bulbmid += '\033[1;37;40m-'
+
+    #printing thermometer
+    print('  ' + glasscap + glasscap)
+    print(bulbedge)
+    print(bulbmid)
+    print(bulbedge)
+    print('  ' + glasscap + glasscap)
+
+    #outputting useful data
+    print('The weather in ' + data['name'] + ', ' + data['sys']['country'] + ' today is: ' + data['weather'][0]['description'])
+    print('high temp: \t' + str(data['main']['temp_max'] - 273.15) + 'C')
+    print('low temp: \t' + str(data['main']['temp_min'] - 273.15) + 'C')
+    print('rel humidity: \t' + str(data['main']['humidity']) + '%')
+
+#main:i
+def main():
+    #default city is Toronto
+    city = "Toronto"
+    if(len(sys.argv) > 1):
+        city = sys.argv[1]
+
+    data = loadData(city)
+    printResults(data)
+
+
+#load main function
+if __name__ == "__main__":
+    main()
